47438d7e7daf959c5e23ca8c3877c824e0bd033c
Grush-cloud <georgeashinze@gmail.com>
2021-03-06 23:13:50 +0400

    javascript file for lessons array

---
+++ lessons.js
@@ -0,0 +1,92 @@
+lessons=[
+    {
+    id:1001,
+    subject: "Art",
+    location: "London",
+    price: 500,
+    spaces:5,
+    image:"art.jpg",
+    
+},
+{
+    id:1002,
+    subject: "Biology",
+    location: "United States",
+    price: 200,
+    spaces:5,
+    image:"biology.jpg",
+    
+},
+{
+    id:1003,
+    subject: "Chemistry",
+    location: "London",
+    price: 1000,
+    spaces:5,
+    image:"chem.jpg",
+    
+},
+{
+    id:1004,
+    subject: "English",
+    location: "United Kingdom",
+    price: 2200,
+    spaces:5,
+    image:"english.jpg",
+    
+},
+{
+    id:1005,
+    subject: "French",
+    location: "Paris",
+    price: 2000,
+    spaces:5,
+    image:"french.jpg",
+    
+},
+{
+    id:1006,
+    subject: "Geography",
+    location: "London",
+    price: 2030,
+    spaces:5,
+    image:"geography.jpg",
+    
+},
+{
+    id:1007,
+    subject: "History",
+    location: "Ghana",
+    price: 1000,
+    spaces:5,
+    image:"history.jpg",
+    
+},
+{
+    id:1008,
+    subject: "Maths",
+    location: "London",
+    price: 2400,
+    spaces:5,
+    image:"maths.jpg",
+    
+},
+{
+    id:1009,
+    subject: "Music",
+    location: "Paris",
+    price: 234,
+    spaces:5,
+    image:"music.jpg",
+    
+},
+{
+    id:1010,
+    subject: "Physics",
+    location: "London",
+    price: 1400,
+    spaces:5,
+    image:"physics.jpg",
+    
+},
+]
